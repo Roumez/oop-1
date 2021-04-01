a502a01f4d8ae2402e69e09a97f3a648c1e9708b
Roumez <remi.roumezy@hotmail.fr>
2021-04-01 18:59:49 +0200

    à vérifier

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-const { Person } = require('./classInfo')
+// const { Person } = require('./classInfo')
 
-const alice = new Person('Alice', 'Liddell', 28, ['solidity', 'react', 'bootstrap'])
-const bob = new Person('Bob', 'Lemon', 30, ['Javascript', 'Node.js'])
-const charlie = new Person('Charlie', 'Charlot', 8, ['html', 'CSS'],)
+// const alice = new Person('Alice', 'Liddell', 28, ['solidity', 'react', 'bootstrap'])
+// const bob = new Person('Bob', 'Lemon', 30, ['Javascript', 'Node.js'])
+// const charlie = new Person('Charlie', 'Charlot', 8, ['html', 'CSS'],)
 
-console.log(alice.printInfo())
-console.log(charlie.canVote())
-console.log(bob.mostSkilledDev(charlie))
+// console.log(alice.printInfo())
+// console.log(charlie.canVote())
+// console.log(bob.mostSkilledDev(charlie))
+
+
+const { P } = require('./point.js')
+
+// p1 est une instance de Point
+const p1 = new P(1, 2)
+// p2 est une autre instance de Point
+const p2 = new P(10, 11)
+
+console.log(p1.show())
+console.log(p1.isEqual(p2))
+console.log(p1.distance(p2))
